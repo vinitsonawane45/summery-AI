dde82d35394323956b4c551fd065e8d9581be28e
vinitsonawane45 <158251287+vinitsonawane45@users.noreply.github.com>
2025-03-26 21:20:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -549,4 +549,4 @@ def sentiment():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3306, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
